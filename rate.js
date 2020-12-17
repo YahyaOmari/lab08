8604293be0b718880db9cfdc0c837daa7859cebc
YahyaOmari <yahya.omari@outlook.com>
2020-12-17 13:32:32 +0200

    updated
    git commit -m updated
    clear

--- rate.js
+++ rate.js
@@ -1,40 +1,45 @@
 //Simple loops
-function giveRate(){
-    let feedback = prompt("Out of 5, how much do you rate us?");
-    let rate;
 
-    if (feedback == 5 || feedback == 4)
-    {
-        rate=alert('Thank you for the good rate.');
-    } else if (feedback >= 0 && feedback <= 3)
-    {
-        rate=alert('Thank you, we will try to fix everything');
-    } else
-    {
-        rate=alert('Something went wrong, you should choose between 0 and 5');
-    }
-    document.write(rate);
-}
+// function giveRate(){
+//     let feedback = prompt("Out of 5, how much do you rate us?");
+//     let rate;
+
+//     if (feedback == 5 || feedback == 4)
+//     {
+//         rate=alert('Thank you for the good rate.');
+//     } else if (feedback >= 0 && feedback <= 3)
+//     {
+//         rate=alert('Thank you, we will try to fix everything');
+//     } else
+//     {
+//         rate=alert('Something went wrong, you should choose between 0 and 5');
+//     }
+//     document.write(rate);
+// }
 
 //Loops with pictures
 
 function getItem()
 {
     let userChoose = prompt("Please type what do you want to play Treadmill , Indoor cycling or Stair climber?");
-    let exrsice= '';
+    var exrsice= '';
 
     while(userChoose !== "Treadmill" && userChoose !== "Indoor cycling" && userChoose !== "Stair climber")
     {
         userChoose=prompt("Please enter what you want to play Treadmill , Indoor cycling or Stair climber ");
     }
+    
+    //console.log('user choice', userChoose, 'exercise', exercise);
+
     if (userChoose == 'Indoor cycling')
     
-    { //console.log('user choice', userChoose, 'exercise', exercise)
+    {
         exrsice = '<img src="Images/cycling.jpg" />';
 
-    } else if (userChoose == 'Indoor climber')
+    } 
+    else if (userChoose == 'Indoor climber')
     {
-        exrsice = '<img src="images/Climber.jpg"/>';
+        exrsice = '<img src="Images/Climber.jpg"/>';
     } 
     else if (userChoose == 'Treadmill')
     {
@@ -56,11 +61,11 @@ function howMany()
 }
 
 function showOrder() {
-    let exrsice = getItem();
+    var exrsice = getItem();
     let total = howMany();
     let result = '';
   
-    for(let i = 0; i < total; i++) {
+    for(let i = 0; i < total; ++i) {
       result = result + '<p>Model #' + i + ' ' + exrsice + '</p>';
     }
     
